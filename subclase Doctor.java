2802aaaca38a40d1f6b5e6486806c2df688b9451
Sunn056 <edwinpvega8@gmail.com>
2024-11-08 14:57:07 -0600

    Subclase Doctor.java

--- subclase Doctor.java
+++ subclase Doctor.java
@@ -1,33 +1,6 @@
-public abstract class Persona {
-    private String nombre;
-    private int edad;
+// Subclase Doctor hecho por Selvin Castellanos
 
-    // Constructor
-    public Persona(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
-    }
 
-    // Métodos getter y setter
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    // Método abstracto
-    public abstract void realizarTrabajo();
-}
 public class Doctor extends Persona {
     private String especialidad;
 
